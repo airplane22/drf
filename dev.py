877d84a316f7ac3d2e880a46b1fc29a8ab27fe5e
airplane22 <plrexpress@student.snue.ac.kr>
2020-03-09 02:09:54 +0900

    doskey 설정

---
+++ dev.py
@@ -0,0 +1,11 @@
+import os
+
+# question 이거 원리
+
+os.system('doskey ga= git add .')
+os.system('doskey gp= git push origin master')
+os.system('doskey gs= git status')
+os.system('doskey gl= git log')
+
+os.system('doskey rs=python manage.py runserver')
+os.system('doskey pms=python manage.py shell')
