4f414239177373947e409d44b2ead297fdeb0904
airplane22 <plrexpress@student.snue.ac.kr>
2020-03-09 03:17:19 +0900

    test commit

--- dev.py
+++ dev.py
@@ -7,5 +7,6 @@ os.system('doskey gp= git push origin master')
 os.system('doskey gs= git status')
 os.system('doskey gl= git log')
 
+
 os.system('doskey rs=python manage.py runserver')
 os.system('doskey pms=python manage.py shell')
