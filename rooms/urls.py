23a52f7d0b1a90c91eb6c1a92645b5c23fb2f9e4
airplane22 <plrexpress@student.snue.ac.kr>
2020-03-15 17:58:22 +0900

    #3.1 RoomViewSet permissions

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,9 +1,13 @@
-from rest_framework.routers import DefaultRouter
-from django.urls import path
 from . import views, viewsets
+from rest_framework.routers import DefaultRouter
 
 
 app_name = "rooms"
+
+router = DefaultRouter()
+router.register("", views.RoomViewSet)
+
+urlpatterns = router.urls
 
 
 # router = DefaultRouter()
@@ -12,11 +16,11 @@ app_name = "rooms"
 # urlpatterns = router.urls
 
 # urlpatterns = [path("list/", views.list_rooms)]
-urlpatterns = [
+# urlpatterns = [
     # path("", views.rooms_view),
-    path("", views.RoomsView.as_view()),
-    # path("<int:pk>/", views.SeeRoomView.as_view()),  # as_view() : 클래스 함수를 통해 인스턴스 생성
-    path("search/", views.room_search),
-    path("<int:pk>/", views.RoomView.as_view()),  # as_view() : 클래스 함수를 통해 인스턴스 생성
-    ]
+    # path("", views.RoomsView.as_view()),
+    # # path("<int:pk>/", views.SeeRoomView.as_view()),  # as_view() : 클래스 함수를 통해 인스턴스 생성
+    # path("search/", views.room_search),
+    # path("<int:pk>/", views.RoomView.as_view()),  # as_view() : 클래스 함수를 통해 인스턴스 생성
+    # ]
 
--- rooms/views.py
+++ rooms/views.py
@@ -1,14 +1,35 @@
 # from rest_framework.generics import ListAPIView
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 # generics : page 등의 정보를 가짐
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Room
 from .serializers import RoomSerializer
+
+
+class RoomViewSet(ModelViewSet):  # drf viewsets
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+    def get_permissions(self):
+        if self.action == "list" or self.action == "retrieve":  # GET rooms/ GET room/#
+            permission_classes = [permissions.AllowAny]
+        elif self.action == "create":
+            permission_classes = [permissions.IsAuthenticated]
+        else:  # elif self.action == "delete" or self.action == ""
+            permission_classes = [IsOwner]
+
+
+    # anybody can list rooms / only authorized can post
+    # only the owner can update/delete rooms
+    # drf permissions
+
+
+
 
 
 # from rest_framework.decorators import api_view
@@ -57,97 +78,97 @@ from .serializers import RoomSerializer
 #             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class OwnPagination(PageNumberPagination):  # 반복 줄이기 / 공식 문서에서 다른 예시 찾아보기!
-    page_size = 20
+# class OwnPagination(PageNumberPagination):  # 반복 줄이기 / 공식 문서에서 다른 예시 찾아보기!
+#     page_size = 20
 
-
-class RoomsView(APIView):
-
-    def get(self, request):
-        # paginator= PageNumberPagination()
-        # paginator.page_size = 20
-        paginator = OwnPagination()
-        rooms = Room.objects.all()
-        results = paginator.paginate_queryset(rooms, request)  # parsing request - paginator 가 page query argument 찾는다.
-        serializer = RoomSerializer(results, many=True, context={'request':request})  # .data 안써도돼? - response에서 쓴다.
-        # return response(serializer)  # django response 와 rest_framework Response 는 다름!
-        return paginator.get_paginated_response(serializer.data)  # page number, previous/next 기능 활용 위해 paginator response 함께 보냄
-
-    def post(self, request):
-        if not request.user.is_authenticated:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-        # print(request.data)  # dict type, not json 출력됨
-        serializer = RoomSerializer(data=request.data)
-        print(serializer)
-        print(dir(serializer))  # serializer 어트리뷰트(대부분 메서드) 확인 --create, update, save 중요
-        print(serializer.is_valid())  # 전송된 data 가 serializer valid 한지 확인 ( 모든 필드 충족시키는지)
-        if serializer.is_valid():
-            # serializer.create()  # never call create, update method directly
-            room = serializer.save(user=request.user)  # save method 가 create / update call 해준다 + validated_data 자동 전송됨
-            room_serializer = RoomSerializer(room).data
-            return Response(data=room_serializer, status=status.HTTP_200_OK)
-        else:
-            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class SeeRoomView(RetrieveAPIView):  # RetrieveAPIView : read-only. get 요청만 받는다.
-#     queryset = Room.objects.all()  # RetrieveAPIView 기본구조
-#     serializer_class = ReadRoomSerializer
-#     # lookup_url_kwarg = "pkkk"  -url 인자 이름 변경
-
-
-class RoomView(APIView):
-
-    def get_room(self, pk):
-        try:
-            room = Room.objects.get(pk=pk)
-            return room
-        except Room.DoesNotExist:
-            return None
-
-    def get(self, request, pk):  # url 에서 pk 인자 받아서 해당 room 을 get
-        # get, put, delete 메서드 모두에서 활용되는 room object get 부분은 메서드(get_room) 활용하여 단축
-        # try:
-        #     room = Room.objects.get(pk=pk)
-        #     serializer = ReadRoomSerializer(room).data
-        #     return Response(serializer)
-        # except Room.DoesNotExist:
-        #     return Response(status=status.HTTP_404_NOT_FOUND)
-        room = self.get_room(pk)
-        if room is not None:
-            serializer = RoomSerializer(room).data
-            return Response(serializer)
-        else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def put(self, request, pk):
-        room = self.get_room(pk)
-        if room is not None:
-            if room.user != request.user:
-                return Response(status=status.HTTP_403_FORBIDDEN)
-            serializer = RoomSerializer(room, data=request.data, partial=True)
-            # instance 받았으므로 serializer 가 update 호출. partial=True : partial update 가능하게 해줌(required 다 안보내도 update)
-            if serializer.is_valid():
-                room = serializer.save()  # option update 호출
-                return Response(RoomSerializer(room).data)  # obj - ReadRoomSerializer 다시 호출 Response(obj or qs) 안돼!
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            # return Response()
-        else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def delete(self, request, pk):  # serializer 호출 불필요한듯
-        room = self.get_room(pk)
-        if room.user != request.user:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-        if room is not None:
-            room.delete()  # 장고
-            return Response(status=status.HTTP_200_OK)
-        else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def update(self, instance, validated_data):
-        print(instance, validated_data)
+#
+# class RoomsView(APIView):
+#
+#     def get(self, request):
+#         # paginator= PageNumberPagination()
+#         # paginator.page_size = 20
+#         paginator = OwnPagination()
+#         rooms = Room.objects.all()
+#         results = paginator.paginate_queryset(rooms, request)  # parsing request - paginator 가 page query argument 찾는다.
+#         serializer = RoomSerializer(results, many=True, context={'request':request})  # .data 안써도돼? - response에서 쓴다.
+#         # return response(serializer)  # django response 와 rest_framework Response 는 다름!
+#         return paginator.get_paginated_response(serializer.data)  # page number, previous/next 기능 활용 위해 paginator response 함께 보냄
+#
+#     def post(self, request):
+#         if not request.user.is_authenticated:
+#             return Response(status=status.HTTP_401_UNAUTHORIZED)
+#         # print(request.data)  # dict type, not json 출력됨
+#         serializer = RoomSerializer(data=request.data)
+#         print(serializer)
+#         print(dir(serializer))  # serializer 어트리뷰트(대부분 메서드) 확인 --create, update, save 중요
+#         print(serializer.is_valid())  # 전송된 data 가 serializer valid 한지 확인 ( 모든 필드 충족시키는지)
+#         if serializer.is_valid():
+#             # serializer.create()  # never call create, update method directly
+#             room = serializer.save(user=request.user)  # save method 가 create / update call 해준다 + validated_data 자동 전송됨
+#             room_serializer = RoomSerializer(room).data
+#             return Response(data=room_serializer, status=status.HTTP_200_OK)
+#         else:
+#             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# # class SeeRoomView(RetrieveAPIView):  # RetrieveAPIView : read-only. get 요청만 받는다.
+# #     queryset = Room.objects.all()  # RetrieveAPIView 기본구조
+# #     serializer_class = ReadRoomSerializer
+# #     # lookup_url_kwarg = "pkkk"  -url 인자 이름 변경
+#
+#
+# class RoomView(APIView):
+#
+#     def get_room(self, pk):
+#         try:
+#             room = Room.objects.get(pk=pk)
+#             return room
+#         except Room.DoesNotExist:
+#             return None
+#
+#     def get(self, request, pk):  # url 에서 pk 인자 받아서 해당 room 을 get
+#         # get, put, delete 메서드 모두에서 활용되는 room object get 부분은 메서드(get_room) 활용하여 단축
+#         # try:
+#         #     room = Room.objects.get(pk=pk)
+#         #     serializer = ReadRoomSerializer(room).data
+#         #     return Response(serializer)
+#         # except Room.DoesNotExist:
+#         #     return Response(status=status.HTTP_404_NOT_FOUND)
+#         room = self.get_room(pk)
+#         if room is not None:
+#             serializer = RoomSerializer(room).data
+#             return Response(serializer)
+#         else:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#     def put(self, request, pk):
+#         room = self.get_room(pk)
+#         if room is not None:
+#             if room.user != request.user:
+#                 return Response(status=status.HTTP_403_FORBIDDEN)
+#             serializer = RoomSerializer(room, data=request.data, partial=True)
+#             # instance 받았으므로 serializer 가 update 호출. partial=True : partial update 가능하게 해줌(required 다 안보내도 update)
+#             if serializer.is_valid():
+#                 room = serializer.save()  # option update 호출
+#                 return Response(RoomSerializer(room).data)  # obj - ReadRoomSerializer 다시 호출 Response(obj or qs) 안돼!
+#             else:
+#                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#             # return Response()
+#         else:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#     def delete(self, request, pk):  # serializer 호출 불필요한듯
+#         room = self.get_room(pk)
+#         if room.user != request.user:
+#             return Response(status=status.HTTP_403_FORBIDDEN)
+#         if room is not None:
+#             room.delete()  # 장고
+#             return Response(status=status.HTTP_200_OK)
+#         else:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#     def update(self, instance, validated_data):
+#         print(instance, validated_data)
 
 
 @api_view(["GET"])
